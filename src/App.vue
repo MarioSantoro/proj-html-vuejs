407230f9762caede76e003884b6d23416adcef0b
MarioSantoro <mariosantoro124@gmail.com>
2023-06-26 12:32:58 +0200

    created a header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script >
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return {
 
+    }
+  },
+  components: {
+    AppHeader,
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap');
+// Import all of Bootstrap's CSS
+@import "bootstrap/scss/bootstrap";
 @import "./styles/general.scss";
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,83 @@
+<template>
+    <header>
+        <div class="container-nav d-flex justify-content-between align-items-center">
+            <div class="logo">
+                <img src="../assets/logo.svg" alt="Logo web-site">
+            </div>
+            <nav>
+                <ul class="d-flex align-items-center fw-bold">
+                    <li class="text-uppercase h-100" v-for="navItem in listNavBar">
+                        {{ navItem }}
+                    </li>
+                </ul>
+            </nav>
+            <div class="social d-flex align-items-center justify-content-between">
+                <div class="search me-4 ">
+                    <img src="../assets/image (8).svg" alt="">
+                </div>
+                <div class="social-icons d-flex align-items-center">
+                    <i class="fa-brands fa-facebook-f me-4"></i>
+                    <img class="me-4" src="../assets/image (10).svg" alt="">
+                    <img class="me-4" src="../assets/image (11).svg" alt="">
+                    <img class="" src="../assets/image (12).svg" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            listNavBar: [
+                "Home",
+                "Blog",
+                "Pages",
+                "About",
+                "Contacts",
+                "Gallery",
+                "Shop",
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss">
+header {
+    height: 90.98px;
+
+    div.container-nav {
+        height: 100%;
+        padding: 0 40px;
+
+        div.logo {
+            img {
+                width: 187px;
+            }
+
+        }
+
+        ul li {
+            font-size: 13px;
+            margin-right: calc(3.2em / 2);
+        }
+    }
+
+    div.search {
+        padding-right: 20px;
+        border-right: 1px solid #cccccc;
+    }
+
+    div.social-icons {
+        i {
+            font-size: 18px;
+        }
+
+        img {
+            height: 18px;
+            width: 18px;
+        }
+    }
+
+}
+</style>
