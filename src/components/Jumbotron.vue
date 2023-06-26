1ee6606edc1cc2d919aaca10cf0b6fff8d548b0d
MarioSantoro <mariosantoro124@gmail.com>
2023-06-26 13:13:18 +0200

    added content on jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="jumbotron">
-        <img src="../assets/blog01.jpg" alt="">
+        <div class="content-container  d-flex align-items-center h-100">
+            <div class="content text-uppercase">
+                <p>Cinemato Studio</p>
+                <h1>Action And Inspire People</h1>
+                <button class="btn btn-light text-uppercase fw-bold">Read more</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,16 +15,32 @@ export default {
 }
 </script>
 <style lang="scss">
+@use "../styles/utilities/variables.scss" as *;
+
 div.jumbotron {
     height: 787px;
     width: 100%;
+    background-image: url(../assets/blog01.jpg);
+    background-size: cover;
+    color: $ColorWhite;
+    font-weight: bolder;
+    line-height: 69px;
 
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover
+    h1 {
+        width: 547px;
+        font-size: 61px;
+        margin: 0;
     }
 
-    ;
+    p {
+        font-size: 16px;
+        margin: 0;
+    }
+
+    button {
+        border-radius: 0;
+        font-size: 12px;
+        padding: 9px 30px;
+    }
 }
 </style>
